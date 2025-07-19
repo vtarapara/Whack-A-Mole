3a8e7705251e302827a0e3c3ebadb30f494d2d14
vtarapara <vtara321@gmail.com>
2025-07-19 17:36:22 -0400

    monitor score instr

---
+++ testing/monitor_score.s
@@ -0,0 +1,29 @@
+addi $20, $0, 1
+addi $21, $0, 1
+addi $22, $0, 1
+addi $23, $0, 1
+addi $24, $0, 1
+addi $25, $0, 1
+addi $26, $0, 1
+addi $27, $0, 1
+nop
+nop
+nop
+score:
+monitor 0
+score 0
+monitor 1
+score 1
+monitor 2
+score 2
+monitor 3
+score 3
+monitor 4
+score 4
+monitor 5
+score 5
+monitor 6
+score 6
+monitor 7
+score 7
+j score
